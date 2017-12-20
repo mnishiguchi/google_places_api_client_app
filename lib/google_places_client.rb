94ffa92ea08d5cf7fb454ec754e5502a0782e9b5
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-12-20 16:32:01 -0500

    Improve GooglePlacesClient and add GoogleMapsClient
    + Delegate missing to @client
    + Add .slice_objects

--- lib/google_places_client.rb
+++ lib/google_places_client.rb
@@ -1,38 +1,22 @@
+# https://github.com/qpowell/google_places
+# Search results are an array of GooglePlaces::Spot objects.
+# For array of hashes, call `#as_json` or `.format_spots`.
 class GooglePlacesClient
+  # Make all the client's methods available.
+  delegate_missing_to :@client
+
   def initialize
-    # https://github.com/qpowell/google_places
     @client = GooglePlaces::Client.new ENV["GOOGLE_MAPS_API_KEY"]
   end
 
-  # Returns search results for schools in array of hashes.
-  # Radius is in meters.
-  def fetch_schools(lat:, lon:, radius: 10_000)
-    results = @client.spots(lat, lon, types: [:school, :university], radius: radius)
-    format_results_json(results)
-  end
+  class << self
+    # Converts an object array into an array of sliced hashes.
+    def format_spots(spots, slice_keys = %w[name lat lng types vicinity])
+      return unless spots.first.is_a? GooglePlaces::Spot
 
-  # def fetch_place_details(place_id)
-  #   @client.spot(place_id)
-  # end
-  #
-  # def fetch_photo_url(place_id, max_width: 800)
-  #   fetch_place_details(place_id).photos[0].fetch_url(max_width)
-  # end
-
-  private
-
-  def format_results_json(results)
-    results.map do |result|
-      {
-        name: result.name,
-        place_id: result.place_id,
-        reference: result.reference,
-        types: result.types,
-        formatted_address: result.formatted_address,
-        lat: result.lat,
-        lng: result.lng,
-        viewport: result.viewport
-      }
+      spots.map do |spot|
+        slice_keys.map { |slice_key| [slice_key, spot.send(slice_key)] }.to_h
+      end
     end
   end
 end
